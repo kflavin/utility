b9ba5ffcaaea6b8ccbbaba5b3004c8f27b8aeacf
Kyle Flavin <Kyle.Flavin@citrix.com>
2016-06-03 16:48:28 -0700

    Added a retry function for Foreman calls.

--- check-puppet-agent.py
+++ check-puppet-agent.py
@@ -23,6 +23,7 @@ import os
 import socket
 import csv
 import requests
+import time
 requests.packages.urllib3.disable_warnings()
 
 f = open("failed_splunk_upgrades.csv", 'wt')
@@ -31,6 +32,27 @@ writer.writerow(("Hostname", "IP Address", "Hostgroup", "Last Puppet Run", "Pupp
 
 base_path = "pssh3/"
 puppet_class = "splunk::forwarder"
+
+# Foreman is throwing 500 errors sometimes, so adding a retry...
+def call_foreman(foreman, function, params):
+  f = getattr(foreman, function)
+  notdone = True
+  counter = 0
+  count = 10
+  while notdone:
+    print "not done"
+    try:
+      if params:
+        val = f(**params)
+      else:
+        val = f()
+      notdone = False
+    except:
+      time.sleep(1)
+      notdone = True
+      counter += 1
+      if counter >= 10: raise
+  return val
 
 for ip in os.listdir(base_path):
   # get hostname
@@ -59,12 +81,15 @@ for ip in os.listdir(base_path):
       stuck_puppet = True if stuck_puppet.startswith("locked") else False
 
   # get foreman info.  first try to look up by IP, then by name.
+  print "here"
   foreman = Foreman(os.environ['foreman_host'], (os.environ['foreman_user'], os.environ['foreman_password']), api_version="2")
-  h = foreman.index_hosts(search="ip=%s" % ip)
+  print "here2"
+  h = call_foreman(foreman, "index_hosts", {"search": "ip=%s" % ip})
+  print "here3"
 
   if h.get("subtotal") == 1:
     h_id = h.get("results")[0]['hostgroup_id']
-    hg = foreman.show_hostgroups(id=h_id)
+    hg = call_foreman(foreman, "show_hostgroups", {"id": h_id})
     if hg:
       base_group = hg['title']
     else:
@@ -77,10 +102,10 @@ for ip in os.listdir(base_path):
     foreman_status = "more than one host found"
   else:
 
-    h = foreman.index_hosts(search="name=%s" % hostname)
+    h = call_foreman(foreman, "index_hosts", {"search": "name=%s" % hostname})
     if h.get("subtotal") == 1:
       h_id = h.get("results")[0]['hostgroup_id']
-      hg = foreman.show_hostgroups(id=h_id)
+      hg = call_foreman(foreman, "show_hostgroups", {"id": h_id})
       if hg:
         base_group = hg['title']
       else:
@@ -101,15 +126,29 @@ for ip in os.listdir(base_path):
        groups = [base_group]
 
     # Get all hostgroups with our class and put them in a list
-    hg = foreman.index_hostgroups(search="class={0}".format(puppet_class))
+    hg = call_foreman(foreman, "index_hostgroups", {"search": "class={0}".format(puppet_class)})
     if hg['subtotal'] > hg['per_page']: print "***DID NOT RETRIEVE ALL PAGES***"
-    hg_names = [ hostgroup['name'] for hostgroup in hg['results'] ]
+    hg_titles = [ hostgroup['title'] for hostgroup in hg['results'] ]
 
     # see if our hostgroup has the class
     sf_class_flag = False
     for group in groups:
-      if group in hg_names:
+      if group in hg_titles:
         sf_class_flag = True
+
+    # check for inheritance
+    if not sf_class_flag:
+      while True:
+        parent = "/".join(base_group.split("/")[:-1])
+        print "parent", parent
+        if parent:
+          if parent in hg_titles:
+            sf_class_flag = True
+            break
+        else:
+          break
+        if parent.find("/") == -1:
+          break
 
   writer.writerow((hostname, ip, base_group, puppet_run, puppet_status, stuck_puppet, foreman_status, sf_class_flag, "" if ro_mounts == "noro" else ro_mounts))
 
