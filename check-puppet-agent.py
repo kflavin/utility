550a844fb2cc3175ac2717b950fcb487bef94cd0
Kyle Flavin <Kyle.Flavin@citrix.com>
2016-06-06 10:34:49 -0700

    Altered pssh file format to add additional data

--- check-puppet-agent.py
+++ check-puppet-agent.py
@@ -26,11 +26,18 @@ import requests
 import time
 requests.packages.urllib3.disable_warnings()
 
+
+# pssh
+# pssh -p 40 -h hosts -l <username> -o pssh5 -A 'set -o pipefail; grep failed: /var/lib/puppet/state/last_run_summary.yaml 2>/dev/null | tr -d " " || echo nofail; test -e /var/lib/puppet/state/agent_disabled.lock; echo disabled:$?; grep ro, /proc/mounts | grep -v /proc | grep -v /dev/loop | grep -v /sys | grep -v nfs | tr "\n" ":" && echo || echo noro; found=$(find /var/lib/puppet/state -name agent_catalog_run.lock -mmin +240); ( test -z $found && echo nolock ) || echo locked; uname -n'
+
+# clean files
+# sed -i -e '/^Unable to create\.*/d' -e '/^Creating directory.*/d' *
+
 f = open("failed_splunk_upgrades.csv", 'wt')
 writer = csv.writer(f)
 writer.writerow(("Hostname", "IP Address", "Hostgroup", "Last Puppet Run", "Puppet Status", "Stuck Puppet", "Foreman Status", "Splunk::Forwarder", "Comments"))
 
-base_path = "pssh3/"
+base_path = "pssh6/"
 puppet_class = "splunk::forwarder"
 
 # Foreman is throwing 500 errors sometimes, so adding a retry...
@@ -40,7 +47,6 @@ def call_foreman(foreman, function, params):
   counter = 0
   count = 10
   while notdone:
-    print "not done"
     try:
       if params:
         val = f(**params)
@@ -51,8 +57,39 @@ def call_foreman(foreman, function, params):
       time.sleep(1)
       notdone = True
       counter += 1
-      if counter >= 10: raise
+      if counter >= 10:
+        val = "foreman unresponsive"
+        break
   return val
+
+
+# Check Foreman return values
+def check_values(h):
+
+  if h.get("subtotal") == 1:
+    h_id = h.get("results")[0]['hostgroup_id']
+    hg = call_foreman(foreman, "show_hostgroups", {"id": h_id})
+    if hg:
+      base_group = hg['title']
+    else:
+      base_group = "None"
+
+    foreman_status = "found"
+    retry = False
+
+  elif h.get("subtotal") > 1:
+    base_group = ""
+    foreman_status = "more than one host found"
+    retry = False
+  else:
+    base_group = "not found"
+    foreman_status = "not found"
+    retry = True
+
+  return retry, base_group, foreman_status
+  
+# get foreman info.  first try to look up by IP, then by DNS name, and finally use name from host.
+foreman = Foreman(os.environ['foreman_host'], (os.environ['foreman_user'], os.environ['foreman_password']), api_version="2")
 
 for ip in os.listdir(base_path):
   # get hostname
@@ -64,11 +101,13 @@ for ip in os.listdir(base_path):
 
   # get puppet info
   if os.stat("%s/%s" % (base_path, ip,)).st_size == 0:
-    puppet_run = puppet_status = ro_mounts = stuck_puppet = "login failed"
+    puppet_run = puppet_status = stuck_puppet = "login failed"
+    ro_mounts = ""
+    local_hostname = ""
   else:
     with open("%s/%s" % (base_path, ip,)) as f2:
-      print ip
-      run_status, agent_disabled, ro_mounts, stuck_puppet  = f2.read().strip().split("\n")
+      print ip, local_hostname
+      run_status, agent_disabled, ro_mounts, stuck_puppet, local_hostname  = f2.read().strip().split("\n")
 
       if run_status.startswith("nofail"):
          puppet_run = ""
@@ -80,42 +119,57 @@ for ip in os.listdir(base_path):
 
       stuck_puppet = True if stuck_puppet.startswith("locked") else False
 
-  # get foreman info.  first try to look up by IP, then by name.
-  print "here"
-  foreman = Foreman(os.environ['foreman_host'], (os.environ['foreman_user'], os.environ['foreman_password']), api_version="2")
-  print "here2"
+
+  # try IP
   h = call_foreman(foreman, "index_hosts", {"search": "ip=%s" % ip})
-  print "here3"
+  retry, base_group, foreman_status = check_values(h)
 
-  if h.get("subtotal") == 1:
-    h_id = h.get("results")[0]['hostgroup_id']
-    hg = call_foreman(foreman, "show_hostgroups", {"id": h_id})
-    if hg:
-      base_group = hg['title']
+  if retry: 
+    if hostname:
+      # try DNS hostname
+      h = call_foreman(foreman, "index_hosts", {"search": "name=%s" % hostname})
+      retry, base_group, foreman_status = check_values(h)
+      foreman_status += " (used dns name)"
     else:
-      base_group = "None"
-
-    foreman_status = "found"
-
-  elif h.get("subtotal") > 1:
-    base_group = ""
-    foreman_status = "more than one host found"
+      if local_hostname:
+        # try local hostname
+        h = call_foreman(foreman, "index_hosts", {"search": "name=%s" % local_hostname})
+        retry, base_group, foreman_status = check_values(h)
+        foreman_status += " (used local hostname)"
+        hostname = local_hostname
   else:
+    foreman_status += " (used ip)"
 
-    h = call_foreman(foreman, "index_hosts", {"search": "name=%s" % hostname})
-    if h.get("subtotal") == 1:
-      h_id = h.get("results")[0]['hostgroup_id']
-      hg = call_foreman(foreman, "show_hostgroups", {"id": h_id})
-      if hg:
-        base_group = hg['title']
-      else:
-        base_group = "None"
-    elif h.get("subtotal") > 1:
-      base_group = ""
-      foreman_status = "more than one host found (by name)"
-    else:
-      base_group = ""
-      foreman_status = "host not found (by name)"
+
+  #if h.get("subtotal") == 1:
+  #  h_id = h.get("results")[0]['hostgroup_id']
+  #  hg = call_foreman(foreman, "show_hostgroups", {"id": h_id})
+  #  if hg:
+  #    base_group = hg['title']
+  #  else:
+  #    base_group = "None"
+
+  #  foreman_status = "found"
+
+  #elif h.get("subtotal") > 1:
+  #  base_group = ""
+  #  foreman_status = "more than one host found"
+  #else:
+
+  #  h = call_foreman(foreman, "index_hosts", {"search": "name=%s" % hostname})
+  #  if h.get("subtotal") == 1:
+  #    h_id = h.get("results")[0]['hostgroup_id']
+  #    hg = call_foreman(foreman, "show_hostgroups", {"id": h_id})
+  #    if hg:
+  #      base_group = hg['title']
+  #    else:
+  #      base_group = "None"
+  #  elif h.get("subtotal") > 1:
+  #    base_group = ""
+  #    foreman_status = "more than one host found (by name)"
+  #  else:
+  #    base_group = ""
+  #    foreman_status = "host not found (by name)"
 
   # If there's a base group, see if it has the correct class
   if base_group != "None":
@@ -140,7 +194,6 @@ for ip in os.listdir(base_path):
     if not sf_class_flag:
       while True:
         parent = "/".join(base_group.split("/")[:-1])
-        print "parent", parent
         if parent:
           if parent in hg_titles:
             sf_class_flag = True
